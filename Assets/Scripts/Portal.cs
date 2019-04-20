cbfcd4d96b207fc9006417dc3c0f84216c980000
Yun Hsiao Wu <yunhsiaow@gmail.com>
2019-04-20 16:40:54 +0800

    first working version

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    SpriteRenderer sprite;
+    PortalManager manager;
+    Color color_active = new Color(0, 0.6f, 0);
+    Color color_inactive = new Color(0.6f, 0, 0);
+
+    // Start is called before the first frame update
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+        manager = GetComponentInParent<PortalManager>();
+    }
+
+    // Update is called once per frame
+    void SetActive(bool active) {
+        sprite.color = active ? color_active : color_inactive;
+    }
+
+    public bool GetCenterInClientRect(Camera camera, out Vector3 screen_pos) {
+        screen_pos = transform.position;
+        screen_pos -= camera.transform.position;
+        screen_pos.x = 1.0f + screen_pos.x / (camera.orthographicSize * camera.aspect);
+        screen_pos.y = 1.0f - screen_pos.y / camera.orthographicSize;
+        if (screen_pos.x < -0.1f || screen_pos.x > 2.1f || screen_pos.y < -0.1f || screen_pos.x > 2.1f)
+            return false; // outside current window boundary
+        screen_pos.x *= camera.pixelWidth * 0.5f;
+        screen_pos.y *= camera.pixelHeight * 0.5f;
+        return true;
+    }
+
+    public Vector3 GetHalfExtentsOnScreen(Camera camera) {
+        // get world space size (this version handles rotating correctly)
+        Vector2 sprite_size = sprite.sprite.rect.size;
+        Vector2 local_sprite_size = sprite_size / sprite.sprite.pixelsPerUnit;
+        Vector3 world_size = local_sprite_size;
+        world_size.x *= transform.lossyScale.x;
+        world_size.y *= transform.lossyScale.y;
+
+        //convert to screen space size
+        Vector3 screen_size = 0.5f * world_size / camera.orthographicSize;
+        screen_size.x /= camera.aspect;
+
+        //size in pixels
+        screen_size.x *= camera.pixelWidth * 0.5f;
+        screen_size.y *= camera.pixelHeight * 0.5f;
+        return screen_size;
+    }
+
+    /* */
+    private void Update() {
+        Portal p = manager.GetConnectedPortal(this);
+        if (!p) { SetActive(false); return; }
+        SetActive(true); p.SetActive(true);
+    }
+    /* */
+
+    void OnCollisionExit(Collision collision) {
+        Portal p = manager.GetConnectedPortal(this);
+        if (!p) return; // die
+        // teleport
+    }
+}
---
+++ Assets/Scripts/WindowManager.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+
+class WindowMessage : NSGMessage {
+    public Vector2 pos;
+    public Vector2 size;
+
+    public override void Deserialize(NetworkReader reader) {
+        base.Deserialize(reader);
+        pos = reader.ReadVector2();
+        size = reader.ReadVector2();
+    }
+
+    public override void Serialize(NetworkWriter writer) {
+        base.Serialize(writer);
+        writer.Write(pos);
+        writer.Write(size);
+    }
+}
+
+public class WindowManager : MonoBehaviour {
+
+    [HideInInspector]
+    public List<Vector2> positions = new List<Vector2>();
+    [HideInInspector]
+    public List<Vector2> sizes = new List<Vector2>();
+
+    public NetworkAgent network;
+
+    // https://answers.unity.com/questions/915069/want-to-get-the-location-of-the-gameview-window.html
+    #if UNITY_STANDALONE_WIN || UNITY_EDITOR
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT {
+            public int X;
+            public int Y;
+            public static implicit operator Vector2(POINT p) {
+                return new Vector2(p.X, p.Y);
+            }
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+        public static Vector2 GetWindowPos() {
+            Vector2 inputCursor = Input.mousePosition;
+            // flip input Cursor y (as the Reference "0" is the last scanline)
+            inputCursor.y = Screen.height - 1 - inputCursor.y;
+            GetCursorPos(out POINT p);
+            return p - inputCursor;
+        }
+    #endif
+
+    public void SetPosition(int idx, float x, float y) {
+        while (positions.Count <= idx) positions.Add(new Vector2());
+        positions[idx] = new Vector2(x, y);
+    }
+
+    public void SetSize(int idx, float width, float height) {
+        while (sizes.Count <= idx) sizes.Add(new Vector2());
+        sizes[idx] = new Vector2(width, height);
+    }
+    
+    void Start() {
+        network.RegisterHandler<WindowMessage>(NSGMsgType.Window, OnWindowMsg);
+    }
+
+    void Update() {
+        var pos = GetWindowPos();
+        SetPosition(network.connID, pos.x, pos.y);
+        SetSize(network.connID, Screen.width, Screen.height);
+        SendWindowMsg(positions[network.connID], sizes[network.connID]);
+    }
+
+    void SendWindowMsg(Vector2 pos, Vector2 size) {
+        if (!NetworkClient.active) return;
+        WindowMessage msg = new WindowMessage {
+            connID = network.connID,
+            pos = pos,
+            size = size
+        };
+        network.client.Send(NSGMsgType.Window, msg);
+    }
+
+    void OnWindowMsg(WindowMessage msg) {
+        SetPosition(msg.connID, msg.pos.x, msg.pos.y);
+        SetSize(msg.connID, (int)msg.size.x, (int)msg.size.y);
+    }
+
+    void OnGUI() {
+        int ypos = 0;
+        for (int i = 0; i < positions.Count; i++) {
+            GUI.Label(new Rect(200, ypos += 20, 300, 20), 
+                positions[i] + " " + sizes[i]);
+        }
+    }
+}
