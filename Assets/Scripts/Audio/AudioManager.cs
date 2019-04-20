b8edcd2dace87aa2b6c899735eb586ab600b8608
Jayce_Lai <goodlaijiaxin@qq.com>
2019-04-21 01:56:24 +0800

    增加音乐

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public List<AudioClip> audioClips;
+    public AudioSource Scene1;
+    public AudioSource Scene2;
+    public AudioSource Scene3;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
