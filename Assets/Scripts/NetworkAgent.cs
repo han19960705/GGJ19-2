eead6c56e3fae5e8ada30177e3a45d5ccf9af282
Yun Hsiao Wu <yunhsiaow@gmail.com>
2019-04-20 09:12:03 +0800

    init

---
+++ Assets/Scripts/NetworkAgent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+public class NetworkAgent : MonoBehaviour {
+
+    NetworkManager manager;
+    NetworkClient client;
+
+    public WindowInfo windowManager;
+    [HideInInspector]
+    public int connID;
+
+    class NSGMsgType {
+        public static short Window = MsgType.Highest + 1;
+    }
+
+    class NSGMessage : MessageBase {
+        public int connID;
+
+        public override void Deserialize(NetworkReader reader) {
+            base.Deserialize(reader);
+            connID = reader.ReadInt32();
+        }
+
+        public override void Serialize(NetworkWriter writer) {
+            base.Serialize(writer);
+            writer.Write(connID);
+        }
+    }
+
+    class WindowMessage : NSGMessage {
+        public Vector2 pos;
+        public Vector2 size;
+
+        public override void Deserialize(NetworkReader reader) {
+            base.Deserialize(reader);
+            pos = reader.ReadVector2();
+            size = reader.ReadVector2();
+        }
+
+        public override void Serialize(NetworkWriter writer) {
+            base.Serialize(writer);
+            writer.Write(pos);
+            writer.Write(size);
+        }
+    }
+
+    // the first instance start as host, rest instances start as clients
+    void Start() {
+        manager = GetComponent<NetworkManager>();
+        client = manager.StartHost();
+        if (!NetworkClient.active) client = manager.StartClient();
+        if (!ClientScene.ready) ClientScene.Ready(manager.client.connection);
+        client.RegisterHandler(MsgType.Connect, OnConnnected);
+        RegisterHandler<WindowMessage>(NSGMsgType.Window, OnWindowMsg);
+    }
+
+    void OnConnnected(NetworkMessage msg) {
+        connID = msg.conn.connectionId;
+    }
+
+    public void SendWindowMsg(Vector2 pos, Vector2 size) {
+        if (!NetworkClient.active) return;
+        WindowMessage msg = new WindowMessage {
+            connID = connID,
+            pos = pos,
+            size = size
+        };
+        client.Send(NSGMsgType.Window, msg);
+    }
+
+    void OnWindowMsg(WindowMessage msg) {
+        windowManager.SetPosition(msg.connID, msg.pos.x, msg.pos.y);
+        windowManager.SetSize(msg.connID, (int)msg.size.x, (int)msg.size.y);
+    }
+
+    /////////////
+    
+    void RegisterHandler<TMsg>(short msgType, Action<TMsg> callback) where TMsg : NSGMessage, new() {
+        NetworkServer.RegisterHandler(NSGMsgType.Window, OnServerMsg<TMsg>);
+        client.RegisterHandler(NSGMsgType.Window, ClientMsgCallbackFactory(callback));
+    }
+
+    NetworkMessageDelegate ClientMsgCallbackFactory<TMsg>(Action<TMsg> callback) where TMsg : NSGMessage, new() {
+        return netMsg => {
+            TMsg msg = netMsg.ReadMessage<TMsg>();
+            if (msg.connID == connID) return;
+            callback(msg);
+        };
+    }
+
+    void OnServerMsg<TMsg>(NetworkMessage netMsg) where TMsg : NSGMessage, new() {
+        TMsg msg = netMsg.ReadMessage<TMsg>();
+        NetworkServer.SendToAll(NSGMsgType.Window, msg);
+    }
+}
+#pragma warning restore CS0618 // Type or member is obsolete
---
+++ Assets/Scripts/WindowInfo.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class WindowInfo : MonoBehaviour {
+
+    const int CLIENT_LIMIT = 2;
+    readonly Vector2[] positions = new Vector2[CLIENT_LIMIT];
+    readonly Vector2[] sizes = new Vector2[CLIENT_LIMIT];
+
+    public NetworkAgent network;
+
+    // https://answers.unity.com/questions/915069/want-to-get-the-location-of-the-gameview-window.html
+    #if UNITY_STANDALONE_WIN || UNITY_EDITOR
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT {
+            public int X;
+            public int Y;
+            public static implicit operator Vector2(POINT p) {
+                return new Vector2(p.X, p.Y);
+            }
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+        public static Vector2 GetWindowPos() {
+            Vector2 inputCursor = Input.mousePosition;
+            // flip input Cursor y (as the Reference "0" is the last scanline)
+            inputCursor.y = Screen.height - 1 - inputCursor.y;
+            GetCursorPos(out POINT p);
+            return p - inputCursor;
+        }
+    #endif
+
+    public void SetPosition(int idx, float x, float y) {
+        positions[idx].x = x;
+        positions[idx].y = y;
+    }
+
+    public void SetSize(int idx, float width, float height) {
+        sizes[idx].x = width;
+        sizes[idx].y = height;
+    }
+
+    void Update() {
+        var pos = GetWindowPos();
+        SetPosition(network.connID, pos.x, pos.y);
+        SetSize(network.connID, Screen.width, Screen.height);
+        network.SendWindowMsg(positions[network.connID], sizes[network.connID]);
+    }
+
+    void OnGUI() {
+        GUI.Label(new Rect(200, 0, 300, 20), positions[0].ToString() + " " + sizes[0].ToString());
+        GUI.Label(new Rect(200, 100, 300, 20), positions[1].ToString() + " " + sizes[1].ToString());
+    }
+}
